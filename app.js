bb2d5eb94c1f94f542e34a02f464ccb8d71fe973
Seong Su Park <sspark114@gmail.com>
2021-07-18 16:19:44 -0700

    Add comments and improve studyset route handler

--- app.js
+++ app.js
@@ -8,38 +8,30 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-// express app settings
 const app = express();
 
-// ejs files uses css from public folder
 app.use(express.static("public"));
 
-// bodyParser
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// app sets view engine for ejs
 app.set('view engine', 'ejs');
 
-// app uses express-session package
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false
 }));
 
-// app uses passport package
 app.use(passport.initialize());
 app.use(passport.session());
 
-// user database
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// schema for studySet
 const studySetSchema = new mongoose.Schema({
   title: String,
   flashCards: [{
@@ -50,14 +42,12 @@ const studySetSchema = new mongoose.Schema({
 
 const StudySet = mongoose.model("StudySet", studySetSchema);
 
-// mongoose schema for user
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   studySets: [studySetSchema]
 });
 
-// used to hash and salt passwords
 userSchema.plugin(passportLocalMongoose);
 
 // mongoose User Model
@@ -68,6 +58,7 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// function that returns a boolean if the request is authenticated
 function getLogin(req) {
   if (req.isAuthenticated()) {
     return true;
@@ -75,168 +66,14 @@ function getLogin(req) {
   return false;
 }
 
+// GET method route for homepage
 app.get("/", function(req, res) {
   res.render("home", {
     login: getLogin(req)
   });
 });
 
-app.route("/register")
-  .get(function(req, res) {
-    res.render("register", {
-      errorMsg: "",
-      login: getLogin(req)
-    });
-  })
-  .post(function(req, res) {
-
-    // if authenticated, logout
-    if (req.isAuthenticated()) {
-      res.logout("/");
-    }
-
-    // retrieves form values
-    console.log(req.body);
-    const username = req.body.username;
-    const password = req.body.password;
-    const password2 = req.body.password2;
-
-    // checks if passwords are equal
-    if (password === password2) {
-      // checks if the username exists creates an account if it doesn't exist
-      User.findOne({
-        username: username
-      }, function(err, foundUser) {
-        if (err) {
-          console.log(err);
-          res.render("register", {
-            errorMsg: ""
-          });
-        } else if (foundUser) {
-          // send error message username already exists
-          res.render("register", {
-            errorMsg: "Username already exists",
-            login: getLogin(req)
-          });
-        } else {
-          // register, authenticate and redirect
-          User.register({
-            username: username
-          }, password, function(err, user) {
-            if (err) {
-              console.log(err);
-              res.render("register", {
-                errorMsg: "",
-                login: getLogin(req)
-              });
-            } else {
-              passport.authenticate("local")(req, res, function() {
-                res.redirect("/latest");
-              });
-            }
-          });
-        }
-      });
-    } else {
-      res.render("register", {
-        errorMsg: "Passwords are not same",
-        login: getLogin(req)
-      });
-    }
-  });
-
-// page for logged in users, shows their studysets
-app.route("/latest")
-  .get(function(req, res) {
-    if (req.isAuthenticated()) {
-      console.log(req.user);
-      User.findOne({
-        username: req.user.username
-      }, function(err, foundUser) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render("latest", {
-            studySets: foundUser.studySets,
-            login: getLogin(req),
-            username: foundUser.username
-          });
-        }
-      });
-    } else {
-      res.redirect("/login");
-    }
-  });
-
-
-// page for creating study sets
-app.route("/creation")
-  .get(function(req, res) {
-    if (req.isAuthenticated()) {
-      res.render("creation", {
-        login: getLogin(req)
-      });
-    } else {
-      res.render("login", {
-        errorMsg: "",
-        login: getLogin(req)
-      });
-    }
-  })
-  .post(function(req, res) {
-    if (req.isAuthenticated()) {
-
-      let data = Object.values(req.body);
-
-      const title = data.shift();
-      let cardList = [];
-      for (let i = 0; i < data.length; i+=2) {
-        if (data[i] != '' && data[i + 1] != '') {
-          // add to list
-          cardList.push({
-            "term": data[i],
-            "termDef": data[i + 1]
-          });
-        }
-      }
-
-      // create StudySet
-      const studySet = new StudySet({
-        title: title,
-        flashCards: cardList
-      });
-
-      studySet.save();
-
-      // push studySet to user
-      User.updateOne(
-        { _id: req.user.id },
-        { $push: {studySets: studySet} },
-        function(err){
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("Succesfully updated User studyset");
-            console.log(req.user.id);
-          }
-        });
-
-
-      res.render("studyset", {
-        cardList: cardList,
-        login: getLogin(req)
-      });
-
-    } else {
-      res.render("login", {
-        errorMsg: "",
-        login: getLogin(req)
-      });
-    }
-  });
-
-
-
+// login page route handler
 app.route("/login")
   .get(function(req, res) {
     if (req.isAuthenticated()) {
@@ -260,7 +97,7 @@ app.route("/login")
           res.redirect("/latest");
         });
       } else {
-        // Username or Password is wrong
+        // renders error Username or Password is wrong
         res.render("login", {
           errorMsg: "Username or Password is wrong",
           login: getLogin(req)
@@ -269,11 +106,160 @@ app.route("/login")
     });
   });
 
+// logout page handler
+app.get("/logout", function(req, res) {
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
+});
 
-// STUDYSET
+// register account page route handler
+app.route("/register")
+  .get(function(req, res) {
+    res.render("register", {
+      errorMsg: "",
+      login: getLogin(req)
+    });
+  })
+  .post(function(req, res) {
+    // if authenticated, logout
+    if (req.isAuthenticated()) {
+      res.logout("/");
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+
+    if (password === password2) {
+      // find if the username exists
+      User.findOne({
+        username: username
+      }, function(err, foundUser) {
+        if (err) {
+          console.log(err);
+          res.render("register", {
+            errorMsg: ""
+          });
+        } else if (foundUser) {
+          // sends error message that username already exists
+          res.render("register", {
+            errorMsg: "Username already exists",
+            login: getLogin(req)
+          });
+        } else {
+          // registers user, authenticates and redirect to latest page
+          User.register({
+            username: username
+          }, password, function(err, user) {
+            if (err) {
+              console.log(err);
+              res.render("register", {
+                errorMsg: "",
+                login: getLogin(req)
+              });
+            } else {
+              passport.authenticate("local")(req, res, function() {
+                res.redirect("/latest");
+              });
+            }
+          });
+        }
+      });
+    } else {
+      // sends error message if passwords not same
+      res.render("register", {
+        errorMsg: "Passwords are not same",
+        login: getLogin(req)
+      });
+    }
+  });
+
+// latest page route handler, shows user's created studysets
+app.route("/latest")
+  .get(function(req, res) {
+    if (req.isAuthenticated()) { // authenticated request required
+      User.findOne({ // finds the user
+        username: req.user.username
+      }, function(err, foundUser) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("latest", {
+            studySets: foundUser.studySets,
+            login: getLogin(req),
+            username: foundUser.username
+          });
+        }
+      });
+    } else { // redirect to login page
+      res.redirect("/login");
+    }
+  });
+
+// creation page for studyset route handler
+app.route("/creation")
+  .get(function(req, res) {
+    if (req.isAuthenticated()) {
+      res.render("creation", {
+        login: getLogin(req)
+      });
+    } else {
+      res.render("login", {
+        errorMsg: "",
+        login: getLogin(req)
+      });
+    }
+  })
+  .post(function(req, res) {
+    if (req.isAuthenticated()) {
+      let formDataList = Object.values(req.body); // turn the form values into an array
+      const title = formDataList.shift();
+      let cardList = []; // empty array for the term - definition pairs
+      for (let i = 0; i < formDataList.length; i+=2) {
+        if (formDataList[i] != '' && formDataList[i + 1] != '') {
+          cardList.push({
+            "term": formDataList[i],
+            "termDef": formDataList[i + 1]
+          });
+        }
+      }
+
+      // create StudySet
+      const studySet = new StudySet({
+        title: title,
+        flashCards: cardList
+      });
+
+      studySet.save();
+
+      // push studySet to user
+      User.updateOne(
+        { _id: req.user.id },
+        { $push: {studySets: studySet} },
+        function(err){
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(req.user.id, " Succesfully updated User studyset");
+          }
+        });
+
+      res.render("studyset", {
+        cardList: cardList,
+        login: getLogin(req)
+      });
+    } else {
+      res.render("login", {
+        errorMsg: "",
+        login: getLogin(req)
+      });
+    }
+  });
+
+// studyset route handler
 app.route("/studyset/:username/:title")
   .get(function(req, res) {
-
     let username = req.params.username;
     let title = req.params.title;
 
@@ -284,21 +270,19 @@ app.route("/studyset/:username/:title")
           let studySet = foundUser.studySets.filter(obj => {
             return obj.title === title;
           });
-          res.render("studyset", {
-            stitle: studySet[0].title,
-            login: getLogin(req)
-          });
+          if (studySet.length !== 0) {
+            res.render("studyset", {
+              stitle: studySet[0].title,
+              login: getLogin(req)
+            });
+          } else {
+            res.redirect("/");
+          }
+        } else {
+          res.redirect("/");
         }
       });
   });
-
-
-
-app.get("/logout", function(req, res) {
-  req.logout();
-  req.session.destroy();
-  res.redirect("/");
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
